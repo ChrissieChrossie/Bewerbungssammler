d40a27cc9d2aa5edc2949128248811ea7162df0e
Christopher <CWolter1@gmx.de>
2026-07-22 09:55:31 +0200

    Fix Terraform

--- terraform/bootstrap/github_oidc.tf
+++ terraform/bootstrap/github_oidc.tf
@@ -36,7 +36,7 @@ resource "aws_iam_role" "github_actions" {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
           }
           StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:${var.github_org}/${var.github_repo}:*"
+            "token.actions.githubusercontent.com:sub" = "repo:${var.github_org}@${var.github_org_id}/${var.github_repo}@${var.github_repo_id}:*"
           }
         }
       }
--- terraform/bootstrap/variables.tf
+++ terraform/bootstrap/variables.tf
@@ -22,6 +22,18 @@ variable "github_repo" {
   default     = "Bewerbungssammler"
 }
 
+variable "github_org_id" {
+  description = "Numerische GitHub-Owner-ID (aus dem OIDC-Sub-Claim, z.B. via Debug-Step ermittelt). Nötig, weil GitHub das 'immutable' Subject-Format mit @<id> nutzt."
+  type        = string
+  default     = "5886958"
+}
+
+variable "github_repo_id" {
+  description = "Numerische GitHub-Repository-ID (aus dem OIDC-Sub-Claim)."
+  type        = string
+  default     = "1306443518"
+}
+
 variable "create_oidc_provider" {
   description = "Ob der GitHub-OIDC-Provider in IAM neu angelegt werden soll. Auf false setzen, falls im AWS-Account schon einer für token.actions.githubusercontent.com existiert."
   type        = bool
